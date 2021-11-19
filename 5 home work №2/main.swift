64ace00100a92f7f1a2d8eb4dca897fa86f3a9b5
keikod1 <madragimova@icloud.com>
2021-11-19 08:43:11 +0600

    home work

--- 5 home work №2/main.swift
+++ 5 home work №2/main.swift
@@ -7,5 +7,38 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Перевод с Usa -> RU")
+
+var translation = readLine()
+
+var world: [String: String] = [
+    "father": "папа",
+    "mother": "мама",
+    "frog": "лягушка",
+    "love": "любовь",
+
+]
+ 
+var wordTranslstion = true
+
+for (key, value) in world{
+    if translation == key{
+        wordTranslstion = true
+        print(value)
+        break
+    }else{
+        wordTranslstion = false
+    }
+}
+
+var worldTranslation2 = true
+
+while worldTranslation2 {
+    if wordTranslstion == false{
+        print("нет такого слова для перевода")
+        break
+    }
+}
+
+
 
